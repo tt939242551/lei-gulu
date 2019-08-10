671f669566c02367f77cc8bc838b1ddd2fd5fcda
xulei <939242551@qq.com>
2019-08-10 20:38:11 +0800

    添加单元测试

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-
+import Chai from 'chai'
+import sinon from 'sinon'
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
-
+const expect = Chai.expect
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
-
-
 
 
 new Vue({
@@ -20,3 +19,70 @@ new Vue({
     loading3: false,
   },
 })
+
+{
+const Constructor = Vue.extend(Button)
+const vm = new Constructor({
+  propsData:{
+    icon:"setting"
+  }
+}).$mount() 
+const useElement = vm.$el.querySelector("use")
+expect(useElement.getAttribute("xlink:href")).to.equal("#i-setting")
+vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:"setting",
+      loading: true
+    }
+  }).$mount() 
+  const useElement = vm.$el.querySelectorAll("use")
+  expect(useElement.length).to.equal(1)
+  expect(useElement[0].getAttribute("xlink:href")).to.equal("#i-loading")
+  vm.$destroy()
+}
+{ 
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:"setting",
+    }
+  }).$mount(div) 
+  const icon = vm.$el.querySelector("svg")
+  expect(getComputedStyle(icon).order).to.equal("1")
+  vm.$el.remove()
+  vm.$destroy()
+}
+{ 
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:"setting",
+      iconPosition: "right"
+    }
+  }).$mount(div) 
+  const icon = vm.$el.querySelector("svg")
+  expect(getComputedStyle(icon).order).to.equal("2")
+  vm.$el.remove()
+  vm.$destroy()
+}
+{ 
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:"setting",
+    }
+  }).$mount() 
+  const callback = sinon.fake()
+  vm.$on('click',callback)
+  vm.$el.click()
+  Chai.assert(callback.called)
+}
--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"
-    @click="$emit('click')">
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true,
+  'disabled':disabled,}"
+    @click="buttonClick">
     <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
@@ -15,9 +16,20 @@
     components: {
       'g-icon': Icon
     },
+    methods:{
+      buttonClick(){
+        if(!this.disabled) {
+          this.$emit('click')
+        }
+      }
+    },
     props: {
       icon: {},
       loading: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
         type: Boolean,
         default: false
       },
@@ -36,7 +48,8 @@
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
   }
-  .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
+  .g-button { font-size: var(--font-size); height: var(--button-height); 
+    padding: 0 1em;
     border-radius: var(--border-radius); border: 1px solid var(--border-color);
     background: var(--button-bg);
     display: inline-flex; justify-content: center; align-items: center;
@@ -45,13 +58,14 @@
     &:active { background-color: var(--button-active-bg); }
     &:focus { outline: none; }
     > .content { order: 2; }
-    > .icon { order: 1; margin-right: .1em; }
+    > .icon { order: 1; margin-right: .4em; }
     &.icon-right {
       > .content { order: 1; }
-      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+      > .icon { order: 2; margin-right: 0; margin-left: .4em;}
     }
     .loading {
       animation: spin 2s infinite linear;
     }
   }
+  .disabled { background-color: var(--button-disabled-bg) }
 </style>
