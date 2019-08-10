237f88bd36880052222f598f0545bfd2ade53d5d
xulei <939242551@qq.com>
2019-08-10 11:43:27 +0800

    添加button组件

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
+
 import Vue from 'vue'
-import gButton from './button.vue'
+import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
+
+
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
+
 
 
 
 new Vue({
   el:"#app",
-  components:{
-    gButton
-  }
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false,
+  },
 })
---
+++ src/icon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+<script>
+  export default {
+    name: 'GuluIcon',
+    props: ['name']
+  }
+</script>
+<style lang="scss" scoped>
+  .g-icon {width: 1em; height: 1em; }
+</style>
