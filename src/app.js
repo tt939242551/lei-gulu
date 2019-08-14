45dd0e3ce9eab7f542f0015832bcab7600c46474
xulei <939242551@qq.com>
2019-08-14 13:13:47 +0800

    完成layout组件

--- src/app.js
+++ src/app.js
@@ -4,12 +4,22 @@ import Button from './button'
 import ButtonGroup from './button-group'
 import Row from './row'
 import Col from './col'
+import Layout from './layout/layout'
+import Header from './layout/header'
+import Sider from './layout/sider'
+import Content from './layout/content'
+import Footer from './layout/footer'
 
 Vue.component('g-button', Button)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
 
 new Vue({
   el:"#app",
---
+++ src/layout/sider.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible=!visible">{{msg}}</button>
+    </div>
+  </transition>
+</template>
+<script>
+  export default {
+    name: 'GuluSider',
+    data () {
+      return {
+        visible: true,
+        msg:"x"
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sider {
+    position: relative;
+    > button {
+      position: absolute;
+      top: 0;
+      right: -10px;
+    }
+  }
+  .slide-enter-active, .slide-leave-active {
+    transition: all .5s;
+  }
+  .slide-enter, .slide-leave-to {
+    margin-left: -150px;
+  }
+</style>
